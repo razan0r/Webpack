78e40bd9522badd76a1d519628b4f3f3c217e5c9
razan0r <razanhamad815@gmail.com>
2024-08-19 11:27:57 +0300

    add webpack.config.js and  Install html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, // cleans the dist folder before each build
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/views/index.html', // Path to your HTML template
+            filename: 'index.html',
+            inject: 'body', // Injects the script tag at the end of the body
+        })
+    ],
+    mode: 'development'
+};
